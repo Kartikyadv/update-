39a549e03beddc8954474db14f5b856c73dd5daf
Kartik Yadav <ky5986390@gmail.com>
2023-04-24 19:59:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Route, Switch} from "react-router-dom";
+import About from './About';
+import Contact from './Contact';
+
+const App = () => {
+  return (<>
+    
+  </>
+  );
+};
+
+export default App;
